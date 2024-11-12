0177388d9a8e243c210d9d10c9f72f37be1784e9
Riccardo Perrone <r.perrone149@gmail.com>
2024-11-12 12:53:26 +0100

    up

--- array.c
+++ array.c
@@ -7,5 +7,11 @@ int main()
     numeri[1] = 4;
     numeri[2] = 89;
     printf("0x = %d\n", &numeri[2], numeri[2]);
+    int anum[] = {1, 4, 3, 57, 6, 3};
+    printf("0x%d = %d\n", &anum[3], anum[3]);
+
+    float dec[] = {1.5, 2.75, 3.14};
+    printf("0x%x = %2.f\n", &dec[0], dec[0]);
+
     return 0;
 }
