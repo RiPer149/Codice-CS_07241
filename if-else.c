41541773b4f67db64a3316f0c9b4acf7c678c33c
Riccardo Perrone <r.perrone149@gmail.com>
2024-11-12 11:54:42 +0100

    up

---
+++ if-else.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int primo;
+    int secondo;
+    int risultato;
+
+    printf("\nInserisci primo numero: ");
+    scanf("%d", &primo);
+
+    printf("\nInserisci secondo numero: ");
+    scanf("%d", &secondo);
+
+    risultato = primo + secondo;
+    if (secondo != 0)
+    {
+        printf("\n%d / %d = %d\n", primo, secondo, primo / secondo);
+        printf("\n%d / %d ha resto %d\n", primo, secondo, primo % secondo);
+    }
+    else
+    {
+        printf("\nNon posso dividere per zero e non mi và di dimostrarti il perchè");
+    }
+    printf("\nHai appena sbloccato una gemma dell'infinito!!!");
+
+    return 0;
+}
