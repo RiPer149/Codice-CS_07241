6d64eb2098df369645191628ba7a11b88022833d
Riccardo Perrone <r.perrone149@gmail.com>
2024-11-12 11:31:56 +0100

    up

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+*questo è il mio codice
+*/
+
+//anche questo è un commento, da poter mettere alla fine di una riga
+
+int main()
+{
+    printf("\nCiao\t\t mondo!\n");
+    return 0;
+}
